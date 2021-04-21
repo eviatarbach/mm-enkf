ce7dddcc1348596a1dd73ae1a4c87c99afc78701
Eviatar Bach <eviatarbach@protonmail.com>
2021-04-21 19:34:31 +0200

    More work

--- ens_forecast.jl
+++ ens_forecast.jl
@@ -20,8 +20,6 @@ struct Forecast_Info
     crps
     crps_uncorr
     spread
-    x_trues
-    errs_m
 end
 
 xskillscore = pyimport("xskillscore")
@@ -35,28 +33,37 @@ function init_ens(; model, integrator, x0, t0, outfreq, Δt, ens_size,
     return E
 end
 
-function mmda(; x0, ensembles::Vector{Array{float_type, 2}}, models, model_true, obs_ops, H, model_errs,
-                integrator,
-                ens_sizes::Vector{Integer}, Δt::float_type, window::Integer, cycles::Integer,
-                outfreq::Integer, D::Integer, k, means, R, ρ) where {float_type<:AbstractFloat}
+function mmda(; x0, ensembles::Vector{Matrix{float_type}}, models, model_true,
+                obs_ops, H, model_errs, integrator, ens_sizes::Vector{Integer},
+                Δt::float_type, window::Integer, n_cycles::Integer,
+                outfreq::Integer, model_sizes::Vector{Integer},
+                R, ρ) where {float_type<:AbstractFloat}
     n_models = length(models)
     obs_err_dist = MvNormal(R)
     R_inv = inv(R)
 
     x_true = x0
 
-    errs = []
-    errs_uncorr = []
-    crps = []
-    crps_uncorr = []
-    x_trues = []
-    errs_m = []
-    spread = []
+    errs = Array{float_type}(undef, n_models, n_cycles)
+    errs_uncorr = Array{float_type}(undef, n_models, n_cycles)
+    crps = Array{float_type}(undef, n_models, n_cycles)
+    crps_uncorr = Array{float_type}(undef, n_models, n_cycles)
+    spread = Array{float_type}(undef, n_models, n_cycles)
 
     t = 0.0
 
-    for cycle=1:cycles
+    for cycle=1:n_cycles
         println(cycle)
+
+        for model=1:n_models
+            E = ensembles[model]
+
+            x_m = mean(E, dims=2)
+            errs_uncorr[model, cycle] = sqrt(mean((x_m .- x_true).^2))
+
+            E_array = xarray.DataArray(data=E, dims=["dim", "member"])
+            crps_uncorr[model, cycle] = xskillscore.crps_ensemble(x_true, E_array).values[1]
+        end
 
         y = H(x_true) + rand(obs_err_dist)
 
@@ -65,6 +72,8 @@ function mmda(; x0, ensembles::Vector{Array{float_type, 2}}, models, model_true,
             # Posterior ensemble of the previous model is used as the prior
             # ensemble for the next model
             E = ensembles[model-1]
+
+            E_model = ensembles[model]
 
             m = ens_sizes[model]
             H_model = obs_ops[model]
@@ -86,25 +95,8 @@ function mmda(; x0, ensembles::Vector{Array{float_type, 2}}, models, model_true,
             # Assimilate the forecast of each ensemble member of the current
             # model as if it were an observation
             for i=1:m
-                E = etkf(E=E, R_inv=P_f_inv, H=H_model, y=E[:, i], Q=Q)
+                E = etkf(E=E, R_inv=P_f_inv, H=H_model, y=E_model[:, i], Q=Q)
             end
-
-            append!(errs_uncorr, sqrt(mean((x_m .- x_true).^2)))
-
-            E_array = xarray.DataArray(data=E, dims=["dim", "member"])
-            E_corr_array = xarray.DataArray(data=E, dims=["dim", "member"])
-            x_m = mean(E, dims=2)
-            append!(errs, sqrt(mean((x_m .- x_true).^2)))
-            append!(crps, xskillscore.crps_ensemble(x_true, E_corr_array).values[1])
-            append!(crps_uncorr, xskillscore.crps_ensemble(x_true, E_array).values[1])
-
-            append!(x_trues, x_true)
-            append!(errs_m, sqrt(mean((means .- x_true).^2)))
-
-            ens_spread = mean(std(E, dims=2))
-            append!(spread, ens_spread)
-
-            x_m = mean(E, dims=2)
 
             ensembles[model] = E
         end
@@ -112,10 +104,15 @@ function mmda(; x0, ensembles::Vector{Array{float_type, 2}}, models, model_true,
         # Assimilate observations
         E_a = etkf(E=ensembles[n_models], R_inv=R_inv, H=H, y=y)
 
-        # Integrate every model
         for model=1:n_models
             # Map from reference model space to the current model space
             E = obs_ops[model]*E_a
+            x_m = mean(E, dims=2)
+
+            errs[model, cycle] = sqrt(mean((x_m .- x_true).^2))
+            E_corr_array = xarray.DataArray(data=E, dims=["dim", "member"])
+            crps[model, cycle] = xskillscore.crps_ensemble(x_true, E_corr_array).values[1]
+            spread[model, cycle] = mean(std(E, dims=2))
 
             for i=1:ens_sizes[model]
                 integration = integrator(models[model], E[:, i], t,
@@ -130,9 +127,7 @@ function mmda(; x0, ensembles::Vector{Array{float_type, 2}}, models, model_true,
         t += window*outfreq*Δt
     end
 
-    x_trues = reshape(x_trues, D, :)
-    return Forecast_Info(errs, errs_uncorr, crps, crps_uncorr, spread,
-                         x_trues, errs_m)
+    return Forecast_Info(errs, errs_uncorr, crps, crps_uncorr, spread)
 end
 
 end
--- mm_da.jl
+++ mm_da.jl
@@ -1,4 +1,5 @@
 using Statistics
+using LinearAlgebra
 
 include("etkf.jl")
 using .ETKF
@@ -15,34 +16,27 @@ using .Integrators
 models = [Models.lorenz63_true, Models.lorenz63_err]
 model_true = Models.lorenz63_true
 n_models = length(models)
+D = 3
 obs_ops = [identity, identity]
+H = identity
+R = diagm(0=>0.1*ones(D))
+model_errs = [zeros(D, D), zeros(D, D)]
 ens_sizes = [20, 20]
+model_sizes = [D, D]
 integrator = rk4
-x0 = rand(3)
+x0 = rand(D)
 t0 = 0.0
 Δt = 0.05
 outfreq = 5
+window = 10
 transient = 200
 ensembles = [init_ens(model=models[model], integrator=integrator, x0=x0, t0=t0,
                       outfreq=outfreq, Δt=Δt, ens_size=ens_sizes[model],
                       transient=transient) for model=1:n_models]
-
 n_cycles = 100
+ρ = 0.5
 
-info = mmda()
-
-
-# for cycle=1:n_cycles
-#     for model=1:n_models-1
-#         x_m = mean(E, dims=2)
-#         X = (E .- x_m)/sqrt(m - 1)
-#         P_f_inv = inv(X*X')
-#         H_m = H
-#         N_m = m
-#         for i=1:N_m
-#             ensembles[model] = etkf(E=ensembles[model], R_inv=P_f_inv, H=H_m, y=E[:, i], Q=Q_m)
-#         end
-#     end
-
-#     E = etkf(E=E, R_inv=R_inv, H=H, y=y)
-# end
+info = mmda(x0, ensembles=ensembles, models=models, model_true=model_true,
+            obs_ops=obs_ops, H=H, model_errs=model_errs, integrator=integrator,
+            ens_sizes=ens_sizes, Δt=Δt, window=window, n_cycles=n_cycles,
+            outfreq=outfreq, model_sizes=model_sizes, R=R, ρ=ρ)
